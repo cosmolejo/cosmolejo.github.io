990e47f431ed0a88eeacfa85e90e4875227fe21b
cosmolejo <almego95@gmail.com>
2018-08-16 14:34:08 -0500

    Add files via upload

---
+++ tensor.sh
@@ -0,0 +1,21 @@
+#######################################
+#
+#
+#	SCRIPT PARA ACTUALIZAR
+#		TENSORFLOW
+#
+#
+########################################
+
+
+######################
+## uninstall
+######################
+pip uninstall tensorflow scipy numpy h5py scikit-learn
+
+
+######################
+## install
+######################
+
+conda install tensorflow scipy numpy h5py scikit-learn
